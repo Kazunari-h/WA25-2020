85055e26d8a6ddb14b88fa74aa0f02e3f0913155
Kazunari-h <kazunario456@gmail.com>
2020-06-26 11:16:17 +0900

    update roulette

--- roulette.js
+++ roulette.js
@@ -3,7 +3,8 @@
 document.getElementById("button")
         .addEventListener("click", function(event) {
             // buttonが押された際の処理
-            console.log("押されたよ");
+            console.log(event);
+            event.target.disabled = true;
 
             // 数字のエリアをドラムロールさせる
             const timer = setInterval(function() {
@@ -16,6 +17,26 @@ document.getElementById("button")
                 clearInterval(timer);
                 const random = Math.floor(Math.random() * 100)
                 document.getElementById("num").innerText = random;
+                event.target.disabled = false;
             }, 3000)
+
+            /*
+            const roulette = function(max) {
+                const random = Math.floor(Math.random() * max)
+                document.getElementById("num").innerText = random;
+            }
+
+            // 数字のエリアをドラムロールさせる
+            const timer = setInterval(function() {
+                roulette(100)
+            }, 50)
+
+            // 3秒後にドラムロールを止めて、抽選した番号を表示する
+            setTimeout(function() {
+                clearInterval(timer);
+                roulette(100)
+                event.target.disabled = false;
+            }, 3000)
+             */
             
         })
