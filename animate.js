5998fad2739967d66a4ef6ba8c231c840eefb4d6
Kazunari-h <kazunario456@gmail.com>
2020-07-03 10:59:02 +0900

    アニメーションの処理を追加

--- animate.js
+++ animate.js
@@ -1,15 +1,29 @@
 const box = document.getElementById("box")
 
 //box.style.borderRadius = "50%"
-let count = 0
+
+// 0-50
+let count_1 = 0
+let increment = true
 
 const timer = setInterval(function () {
-    count++
-    box.style.borderRadius = `${count}%`
-    console.log(count);
+    if (increment) {
+        count_1++
+    } else {
+        count_1--
+    }
+    box.style.borderRadius = `${count_1}%`
+    console.log(count_1);
+
+    if (count_1 < 1 || count_1 > 49) {
+        increment = !increment
+    }
 
     // タイマーを止める処理
-    if (count > 50) {
-        clearInterval(timer)
-    }
-}, 100)
+    // if (count > 50) {
+    //     clearInterval(timer)
+    // }
+}, 20)
+
+
+document.getElementById("box").style.marginTop = "red"
