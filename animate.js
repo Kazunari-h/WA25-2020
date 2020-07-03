5d9fa72671e91ec763857a7f747f39271ca737a2
Kazunari-h <kazunario456@gmail.com>
2020-07-03 12:43:35 +0900

    CSSアニメーションを追加

--- animate.js
+++ animate.js
@@ -1,29 +1,5 @@
 const box = document.getElementById("box")
 
-//box.style.borderRadius = "50%"
-
-// 0-50
-let count_1 = 0
-let increment = true
-
-const timer = setInterval(function () {
-    if (increment) {
-        count_1++
-    } else {
-        count_1--
-    }
-    box.style.borderRadius = `${count_1}%`
-    console.log(count_1);
-
-    if (count_1 < 1 || count_1 > 49) {
-        increment = !increment
-    }
-
-    // タイマーを止める処理
-    // if (count > 50) {
-    //     clearInterval(timer)
-    // }
-}, 20)
-
-
-document.getElementById("box").style.marginTop = "red"
+setTimeout(function () {
+    box.classList.add("animate")
+}, 2000)
